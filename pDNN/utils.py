8814a53e3554f0d1c3f44af0a18c733598d01b80
Yuan Han <44023577+John-Han-0422@users.noreply.github.com>
2024-04-01 15:03:18 +0800

    Update utils.py

--- pDNN/utils.py
+++ pDNN/utils.py
@@ -14,7 +14,7 @@ class config(object):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         # self.dropout = 0.1
         self.require_improvement = 50
-        self.num_epochs = 22000
+        self.num_epochs = 2900
         self.batch_size = 30000
         self.learning_rate = 1e-3
         self.hidden_units = (64, 128, 256, 512, 512, 1024, 1)
